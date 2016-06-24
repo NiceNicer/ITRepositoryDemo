14835c7d9ce6bdc2a5cf7576d101f7b06b3c282a
Cocoacola <mac@CocoaCola.local>
2016-06-24 15:40:07 +0800

    firstChange
    0time

---
+++ ITGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITGitDemo
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 Heng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
